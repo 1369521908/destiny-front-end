c9536bb256b714d17f27b56d29a0ee74fb8855f3
13695 <1369521908@qq.com>
2019-09-06 00:03:40 +0800

    练习

---
+++ src/views/destiny/table/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-container">
+    <el-button type="primary" :loading="loading" @click="handleClick">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false
+    }
+  },
+  mounted() {
+    this.loading = false
+  },
+  methods: {
+    handleClick(done) {
+      this.loading = true
+
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          this.loading = false
+          done()
+        })
+        .catch(e => {
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
